2e4fc6a157e43ae4ac76d140a021981656784755
mdsujan-mridha <mdsujan134523@gmail.com>
2022-08-10 20:42:32 +0600

    json web toekn api created

--- api/controlors/auth.js
+++ api/controlors/auth.js
@@ -1,6 +1,7 @@
 import User from "../models/User.js"
 import bcrypt from "bcryptjs";
 import { createError } from "../errorHandle/error.js";
+import jwt from "jsonwebtoken";
 export const register = async (req, res, next) => {
 
     try {
@@ -23,15 +24,20 @@ export const register = async (req, res, next) => {
 export const login = async (req, res, next) => {
 
     try {
-        const user = await User.findOne({username:req.body.username})
+        const user = await User.findOne({ username: req.body.username })
 
-         if(!user) return next(createError(404,"User not found"));
+        if (!user) return next(createError(404, "User not found"));
 
-     const isPassword = await bcrypt.compare(req.body.password, user.password)
+        const isPassword = await bcrypt.compare(req.body.password, user.password)
 
-         if(!isPassword) return next(createError(400,"Wrong password or username"));
-     const { password,isAdmin, ...otherDetails } = user?._doc;
-     res.status(200).json({...otherDetails});
+        if (!isPassword) return next(createError(400, "Wrong password or username"));
+
+        const token = jwt.sign({ id: user._id, isAdmin: user.isAdmin }, process.env.JWT_TOKEN);
+
+        const { password, isAdmin, ...otherDetails } = user?._doc;
+        res.cookie("Access_token", token, {
+            httpOnly: true,
+        }).status(200).json({ ...otherDetails });
     } catch (err) {
         next(err)
     }
