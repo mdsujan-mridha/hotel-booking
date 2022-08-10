27b88f344994ef0f237dbcd3573769bde52e69d2
mdsujan-mridha <mdsujan134523@gmail.com>
2022-08-10 17:39:05 +0600

    user register api created

---
+++ api/controlors/hotel.js
@@ -0,0 +1,58 @@
+// create a new hotel 
+
+import Hotel from "../models/Hotel.js";
+
+export const createHotel = async(req,res,next)=>{
+    const newHotel = new Hotel(req.body) 
+    try{
+         const savedHotel = await newHotel.save()
+         res.status(200).json(savedHotel)
+      }catch(err){
+          next(err)
+      }
+};
+
+// update a hotel information 
+export const updateHotel = async(req,res,next)=>{
+    const newHotel = new Hotel(req.body) 
+    try{
+         const updateHotel = await Hotel.findByIdAndUpdate(req.params.id,
+            {$set:req.body},
+            {new:true}
+            );
+         res.status(200).json(updateHotel)
+      }catch(err){
+          next(err)
+      }
+    
+};
+
+// delete any hotel 
+export const deleteHotel = async(req,res,next)=>{
+    try{
+        await Hotel.findByIdAndDelete(req.params.id);
+        res.status(200).json("Hotel has been deleted");
+     }catch(err){
+         next(err)
+     }
+};
+
+// get one hotel 
+export const getHotel = async(req,res,next)=>{
+    try{
+        const hotel= await Hotel.findById(req.params.id);
+         res.status(200).json(hotel);
+      }catch(err){
+        next(err)
+      }
+};
+
+// get all hotel 
+export const getHotels = async(req,res,next)=>{
+    try{
+        const hotels = await Hotel.find();
+         res.status(200).json(hotels);
+      }catch(err){
+          next(err)
+      }
+};
